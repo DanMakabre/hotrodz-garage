6474eea13fdc91cd61a8b079e9385c2b5f1342dc
drodriguez <danny.rodriguez@co.hidalgo.tx.us>
2025-01-07 16:53:25 -0600

    commiting tutorial changes

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,9 +1,32 @@
 import React from 'react'
 
-const UsersPage = () => {
-  return (
-    <div>UsersPage</div>
-  )
+interface User {
+    id: number;
+    name: string;
+}
+
+const UsersPage = async () => {
+
+    // const for SQLite3 database named users.db
+    // const db = openDatabase('users.db', '1.0', 'users', 2 * 1024 * 1024);
+
+    const res = await fetch(
+        'https://jsonplaceholder.typicode.com/users', 
+        // { next: { revalidate: 1 } },
+        { cache: "no-store" }
+    );
+    const users: User[] = await res.json();
+    return (
+        <>
+            <h1>Users</h1>
+            <p>{new Date().toLocaleTimeString()}</p>
+            <ul>
+                {users.map(user => (
+                    <li key={user.id}>{user.name}</li>
+                ))}
+            </ul>
+        </>
+    )
 }
 
 export default UsersPage
