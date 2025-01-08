83078692b6ab65f6537c14656e7e22a6a0022219
drodriguez <danny.rodriguez@co.hidalgo.tx.us>
2025-01-08 16:57:52 -0600

    tutorial day 2 (office)

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,30 +1,14 @@
 import React from 'react'
-
-interface User {
-    id: number;
-    name: string;
-}
+import UserTable from './UserTable'
 
 const UsersPage = async () => {
 
     // const for SQLite3 database named users.db
     // const db = openDatabase('users.db', '1.0', 'users', 2 * 1024 * 1024);
-
-    const res = await fetch(
-        'https://jsonplaceholder.typicode.com/users', 
-        // { next: { revalidate: 1 } },
-        { cache: "no-store" }
-    );
-    const users: User[] = await res.json();
     return (
         <>
             <h1>Users</h1>
-            <p>{new Date().toLocaleTimeString()}</p>
-            <ul>
-                {users.map(user => (
-                    <li key={user.id}>{user.name}</li>
-                ))}
-            </ul>
+            <UserTable />
         </>
     )
 }
