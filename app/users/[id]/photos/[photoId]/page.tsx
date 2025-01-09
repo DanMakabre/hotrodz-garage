5caa1851bed01d62344a4b21b4b1c90132d11147
drodriguez <danny.rodriguez@co.hidalgo.tx.us>
2025-01-09 17:00:27 -0600

    day 3 (office)

--- app/users/[id]/photos/[photoId]/page.tsx
+++ app/users/[id]/photos/[photoId]/page.tsx
@@ -4,6 +4,8 @@ interface Props {
     params: { id: number, photoId: number }
 }
 
+// const PhotoPage = (props: Props) => {
+
 const PhotoPage = ({ params: { id, photoId }}: Props) => {
   return (
     <div>PhotoPage {id} {photoId}</div>
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,14 +1,21 @@
 import React from 'react'
 import UserTable from './UserTable'
 
-const UsersPage = async () => {
+
+interface Props {
+    searchParams: { sortOrder: string };
+}
+
+
+const UsersPage = async ({ searchParams: { sortOrder } }: Props) => {
+    console.log(sortOrder)
 
     // const for SQLite3 database named users.db
     // const db = openDatabase('users.db', '1.0', 'users', 2 * 1024 * 1024);
     return (
         <>
             <h1>Users</h1>
-            <UserTable />
+            <UserTable sortOrder={sortOrder} />
         </>
     )
 }
