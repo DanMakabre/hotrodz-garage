23759716f2aa7b13f3d25785d47580049527b87e
drodriguez <danny.rodriguez@co.hidalgo.tx.us>
2025-01-16 16:57:15 -0600

    adding to route

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -3,12 +3,35 @@ import { NextRequest, NextResponse } from 'next/server';
 export function GET(
     request: NextRequest, 
     { params }: { params: { id: number } }) {
-        // fetch data from users.db
-        //  if not found, return 404 error
-        // else return data
-        
         if (params.id > 10)
             return NextResponse.json({ error: 'User not found' }, { status: 404 });
 
-        return NextResponse.json({ id: 1, name: "Shme" });
+        return NextResponse.json({ id: 1, name: "Mosh" });
+    }
+
+
+export async function PUT(
+    request: NextRequest,
+    { params }: { params: { id: number } }) {
+        const body = await request.json();
+        if (!body.name) {
+            return NextResponse.json({ error: 'Name is required' }, { status: 400 });
+        }
+
+        if(params.id > 10) {
+            return NextResponse.json({ error: 'User not found' }, { status: 404 });
+        }
+
+        return NextResponse.json({ id: 1, name: body.name });
+    }
+
+export async function DELETE(
+    request: NextRequest,
+    { params }: { params: { id: number } }) 
+    {
+        if(params.id > 10) {
+            return NextResponse.json({ error: 'User not found' }, { status: 404 });
+        }
+
+        return NextResponse.json({ id: 1, name: "Mosh" });
     }
